868464b1003cab2df54a47ebde23a3589ca44b65
Posthorse <gamewithrown@gmail.com>
2025-01-12 20:40:08 +0900

    ResourcePath 추가

---
+++ Common/ResourcePath/ResourcePath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Systems;
+using Systems.UI;
+
+namespace Utility
+{
+    public static class ResourcePath
+    {
+        public readonly static Dictionary<Type, string> Path = new Dictionary<Type, string>()
+        {
+            {typeof(GlobalCoroutineBehaviour), "Prefab/Global/GlobalCoroutineBehaviour"},
+            {typeof(BattleManager), "Prefab/Global/BattleManager"},
+            {typeof(UIController), "Prefab/Global/UIController"},
+            {typeof(BackGroundManager), "Prefab/Global/BackGroundManager"},
+        };
+
+        public static string GetPath(this Type type)
+        {
+            if (!Path.ContainsKey(type))
+            {
+                LogManager.LogError("No Path Data : {0}".Format(type));
+                return "";
+            }
+
+            return Path[type];
+        }
+    }
+}
